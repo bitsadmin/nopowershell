29805f26a77c5fc8a4b7903a763b8b845b727fa1
bitsadmin <git@myserver.me>
2025-02-26 22:33:52 +0100

    Add 4 cmdlets
    - Get-ADReplicationSubnet
    - Get-Acl
    - ConvertFrom-SddlString
    - Get-FileHash

---
+++ Source/NoPowerShell/Commands/ActiveDirectory/GetADReplicationSubnetCommand.cs
@@ -0,0 +1,87 @@
+﻿using NoPowerShell.Arguments;
+using NoPowerShell.HelperClasses;
+using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+
+/*
+Author: @bitsadmin
+Website: https://github.com/bitsadmin
+License: BSD 3-Clause
+*/
+
+namespace NoPowerShell.Commands.ActiveDirectory
+{
+    public class GetADReplicationSubnetCommand : PSCommand
+    {
+        public GetADReplicationSubnetCommand(string[] userArguments) : base(userArguments, SupportedArguments)
+        {
+        }
+
+        public override CommandResult Execute(CommandResult pipeIn)
+        {
+            // Obtain cmdlet parameters
+            string server = _arguments.Get<StringArgument>("Server").Value;
+            //string searchBase = _arguments.Get<StringArgument>("SearchBase").Value;
+            string identity = _arguments.Get<StringArgument>("Identity").Value;
+            //string ldapFilter = _arguments.Get<StringArgument>("LDAPFilter").Value;
+            List<string> properties = new List<string>(_arguments.Get<StringArgument>("Properties").Value.Split(','));
+
+            // Query
+            string distinguishedName = LDAPHelper.GetDistinguishedName(server, username, password);
+
+            if(string.IsNullOrEmpty(distinguishedName))
+                throw new Exception("Could not determine the distinguished name of the domain");
+
+            string searchBase = $"CN=Subnets,CN=Sites,CN=Configuration,{distinguishedName}";
+            string filter = "(objectClass=subnet)";
+            if (!string.IsNullOrEmpty(identity))
+            {
+                string identityEscaped = identity.Replace("/", "\\2f");
+                filter = $"(&(name={identityEscaped}){filter})";
+            }
+
+            _results = LDAPHelper.QueryLDAP(searchBase, filter, properties, server, username, password);
+
+            return _results;
+        }
+
+        public static new CaseInsensitiveList Aliases
+        {
+            get { return new CaseInsensitiveList() { "Get-ADReplicationSubnet" }; }
+        }
+
+        public static new ArgumentList SupportedArguments
+        {
+            get
+            {
+                return new ArgumentList()
+                {
+                    new StringArgument("Server", true),
+                    //new StringArgument("SearchBase"),
+                    new StringArgument("Identity"),
+                    //new StringArgument("LDAPFilter"),
+                    new StringArgument("Properties", "DistinguishedName,Location,Name,ObjectClass,ObjectGUID,Site", true)
+                };
+            }
+        }
+
+        public static new string Synopsis
+        {
+            get { return "Gets one or more Active Directory subnets."; }
+        }
+
+        public static new ExampleEntries Examples
+        {
+            get
+            {
+                return new ExampleEntries()
+                {
+                    new ExampleEntry("Get all subnets", "Get-ADReplicationSubnet"),
+                    new ExampleEntry("Get subnets with a specified name", "Get-ADReplicationSubnet -Identity \"10.0.10.0/24\""),
+                    new ExampleEntry("Get the properties of a specified subnet", "Get-ADReplicationSubnet -Identity \"10.0.10.0/24\" -Properties *")
+                };
+            }
+        }
+    }
+}
---
+++ Source/NoPowerShell/Commands/Utility/ConvertToCsvCommand.cs
@@ -0,0 +1,90 @@
+﻿using NoPowerShell.Arguments;
+using NoPowerShell.HelperClasses;
+using System.Collections.Generic;
+using System;
+
+/*
+Author: @bitsadmin
+Website: https://github.com/bitsadmin
+License: BSD 3-Clause
+*/
+
+namespace NoPowerShell.Commands.Utility
+{
+    public class ConvertToCsvCommand : PSCommand
+    {
+        public ConvertToCsvCommand(string[] arguments) : base(arguments, SupportedArguments)
+        {
+        }
+
+        public override CommandResult Execute(CommandResult pipeIn)
+        {
+            string delimiter = _arguments.Get<StringArgument>("Delimiter").Value;
+
+            if (pipeIn == null || pipeIn.Count == 0)
+                return null;
+
+            // Compile header
+            string[] columns = new string[pipeIn[0].Count];
+            int i = 0;
+            foreach(KeyValuePair<string, string> col in pipeIn[0])
+            {
+                columns[i] = col.Key;
+                i++;
+            }
+
+            Console.WriteLine("\"{0}\"", (string.Join($"\"{delimiter}\"", columns)));
+
+            // Compile records
+            foreach (ResultRecord result in pipeIn)
+            {
+                // Collect values
+                string[] values = new string[result.Count];
+                i = 0;
+                foreach(KeyValuePair<string, string> kvp in result)
+                {
+                    // Escape quote by double quotes
+                    values[i] = kvp.Value != null ? kvp.Value.Replace("\"", "\"\"") : "";
+                    i++;
+                }
+
+                // Store values
+                Console.WriteLine("\"{0}\"", (string.Join($"\"{delimiter}\"", values)));
+            }
+
+            return null;
+        }
+
+        public static new CaseInsensitiveList Aliases
+        {
+            get { return new CaseInsensitiveList() { "ConvertTo-Csv" }; }
+        }
+
+        public static new ArgumentList SupportedArguments
+        {
+            get
+            {
+                return new ArgumentList()
+                {
+                    new StringArgument("Delimiter", ","),
+                };
+            }
+        }
+
+        public static new string Synopsis
+        {
+            get { return "Converts .NET objects into a series of character-separated value (CSV) strings."; }
+        }
+
+        public static new ExampleEntries Examples
+        {
+            get
+            {
+                return new ExampleEntries()
+                {
+                    new ExampleEntry("Display process list as CSV", @"Get-Process | ConvertTo-Csv"),
+                };
+            }
+        }
+    }
+}
