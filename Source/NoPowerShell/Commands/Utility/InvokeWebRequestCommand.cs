72da5d12a3a6992f21baf958aa46ee7bf5f72f12
bitsadmin <git@myserver.me>
2025-03-31 23:16:02 +0200

    Display certificate chain when using -Verbose flag

--- Source/NoPowerShell/Commands/Utility/InvokeWebRequestCommand.cs
+++ Source/NoPowerShell/Commands/Utility/InvokeWebRequestCommand.cs
@@ -1,10 +1,11 @@
 ﻿using NoPowerShell.Arguments;
 using NoPowerShell.HelperClasses;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 /*
@@ -23,6 +24,10 @@ namespace NoPowerShell.Commands.Utility
 
         public override CommandResult Execute(CommandResult pipeIn)
         {
+            // Obtain generic parameters
+            base.Execute(pipeIn);
+
+            // Obtain Invoke-WebRequest specific parameters
             string uri = _arguments.Get<StringArgument>("URI").Value;
             string useragent = _arguments.Get<StringArgument>("UserAgent").Value;
             string outfile = _arguments.Get<StringArgument>("OutFile").Value;
@@ -36,20 +41,33 @@ namespace NoPowerShell.Commands.Utility
             if (!uri.Contains("://"))
                 uri = "http://" + uri;
 
-            _results = MakeRequest(uri, method, skipCertificateCheck, proxy, proxyCredential, proxyUseDefaultCredentials, useragent, outfile).GetAwaiter().GetResult();
+            _results = MakeRequest(uri, method, skipCertificateCheck, proxy, proxyCredential, proxyUseDefaultCredentials, useragent, outfile, verbose).GetAwaiter().GetResult();
 
             return _results;
         }
 
-        static async Task<CommandResult> MakeRequest(string uri, string method, bool skipCertificateCheck, string proxy, string proxyCredential, bool proxyUseDefaultCredentials, string useragent, string outfile)
+        static async Task<CommandResult> MakeRequest(string uri, string method, bool skipCertificateCheck, string proxy, string proxyCredential, bool proxyUseDefaultCredentials, string useragent, string outfile, bool verbose)
         {
             CommandResult results = new CommandResult();
-
             HttpClientHandler handler = new HttpClientHandler();
 
-            // Skip certificate check
-            if (skipCertificateCheck)
-                ServicePointManager.ServerCertificateValidationCallback = (sender, cert, chain, sslPolicyErrors) => true;
+            // Support displaying certificate chain if -Verbose flag is specified
+            CommandResult certificateChain = new CommandResult();
+            ServicePointManager.ServerCertificateValidationCallback = (sender, cert, chain, sslPolicyErrors) =>
+            {
+                if (verbose)
+                {
+                    foreach (X509ChainElement chainElement in chain.ChainElements)
+                    {
+                        certificateChain.Add(GetCertificateDetails(chainElement.Certificate));
+                    }
+                }
+
+                if (skipCertificateCheck)
+                    return true;
+
+                return sslPolicyErrors == SslPolicyErrors.None;
+            };
 
             // Proxy is specified
             if (!string.IsNullOrEmpty(proxy))
@@ -84,40 +102,57 @@ namespace NoPowerShell.Commands.Utility
                 if (!string.IsNullOrEmpty(useragent))
                     client.DefaultRequestHeaders.Add("User-Agent", useragent);
 
+                HttpResponseMessage response;
                 try
                 {
                     // Download file
                     if (!string.IsNullOrEmpty(outfile))
                     {
                         // Send request
-                        HttpResponseMessage response = await client.SendAsync(request);
+                        response = await client.SendAsync(request);
 
-                        Console.WriteLine(
-                            "Received HTTP/{0} {1}-byte response of content type {2}",
-                            response.Version,
-                            response.Content.Headers.ContentLength,
-                            response.Content.Headers.ContentType.MediaType
-                        );
+                        if (verbose)
+                        {
+                            Program.WriteVerbose(
+                                "Received HTTP/{0} {1}-byte response of content type {2}",
+                                response.Version,
+                                response.Content.Headers.ContentLength,
+                                response.Content.Headers.ContentType.MediaType
+                            );
+                        }
 
                         // Write the content to outfile
                         File.WriteAllBytes(outfile, response.Content.ReadAsByteArrayAsync().GetAwaiter().GetResult());
-
-                        Console.WriteLine("File Name: {0}", outfile);
                     }
                     // Display to console if no outfile is specified
                     else
                     {
                         // Send request
-                        HttpResponseMessage response = await client.SendAsync(request);
-
-                        results.Add(new ResultRecord()
-                        {
-                            { "StatusCode", ((int)response.StatusCode).ToString() },
-                            { "StatusDescription", response.StatusCode.ToString() },
-                            { "Content", await response.Content.ReadAsStringAsync() },
-                            { "Headers", response.Headers.ToString() }
-                        });
+                        response = await client.SendAsync(request);
                     }
+
+                    // Complement with certificate details if in verbose mode
+                    if (verbose)
+                    {
+                        certificateChain.Reverse();
+                        Program.WriteVerbose("Certificate chain:\r\n{0}\r\n", ResultPrinter.FormatList(certificateChain).TrimEnd());
+                    }
+
+                    // Display HTTP response metadata
+                    ResultRecord result = new ResultRecord()
+                    {
+                        { "StatusCode", ((int)response.StatusCode).ToString() },
+                        { "StatusDescription", response.StatusCode.ToString() },
+                        { "Headers", response.Headers.ToString().Trim() }
+                    };
+
+                    // Display HTTP response content or the filename in case -OutFile is specified
+                    if (string.IsNullOrEmpty(outfile))
+                        result.Add("Content", await response.Content.ReadAsStringAsync());
+                    else
+                        result.Add("File Name", outfile);
+
+                    results.Add(result);
                 }
                 catch (HttpRequestException e)
                 {
@@ -129,6 +164,21 @@ namespace NoPowerShell.Commands.Utility
             }
 
             return results;
+        }
+
+        private static ResultRecord GetCertificateDetails(X509Certificate2 certificate)
+        {
+            return new ResultRecord()
+            {
+                {"Subject", certificate.Subject },
+                {"Issuer", certificate.Issuer },
+                {"Valid From", certificate.NotBefore.ToFormattedString() },
+                {"Valid Until", certificate.NotAfter.ToFormattedString() },
+                {"Thumbprint", certificate.Thumbprint },
+                {"Serial Number", certificate.SerialNumber },
+                {"Signature Algorithm", certificate.SignatureAlgorithm.FriendlyName },
+                //{"Public Key", Convert.ToBase64String(certificate.GetPublicKey()) }
+            };
         }
 
         public static new CaseInsensitiveList Aliases
